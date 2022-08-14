34daf05ab2ced3b3643f328a680e296821c4fba5
Saumyajyoti Mukherjee <saumyajyoti@gmail.com>
2022-08-15 00:26:56 +0530

    Update doxygen.lua

--- lua/neogen/templates/doxygen.lua
+++ lua/neogen/templates/doxygen.lua
@@ -1,9 +1,9 @@
 local i = require("neogen.types.template").item
 return {
-    { nil, "/**", { no_results = true, type = { "func", "file", "class" } } },
+    { nil, "/***************************************************************", { no_results = true, type = { "func", "file", "class" } } },
     { nil, " * @file", { no_results = true, type = { "file" } } },
     { nil, " * @brief $1", { no_results = true, type = { "func", "file", "class" } } },
-    { nil, " */", { no_results = true, type = { "func", "file", "class" } } },
+    { nil, " ***************************************************************/", { no_results = true, type = { "func", "file", "class" } } },
     { nil, "", { no_results = true, type = { "file" } } },
 
     { nil, "/**", { type = { "func", "class", "type" } } },
