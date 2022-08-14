bdcbda78fb071c19747b8e7f62e1496b1377faff
Saumyajyoti Mukherjee <saumyajyoti@gmail.com>
2022-08-15 00:32:44 +0530

    Update doxygen.lua

--- lua/neogen/templates/doxygen.lua
+++ lua/neogen/templates/doxygen.lua
@@ -6,7 +6,7 @@ return {
     { nil, " ***************************************************************/", { no_results = true, type = { "func", "file", "class" } } },
     { nil, "", { no_results = true, type = { "file" } } },
 
-    { nil, "/**", { type = { "func", "class", "type" } } },
+    { nil, "/***************************************************************", { type = { "func", "class", "type" } } },
     { i.ClassName, " * @class %s", { type = { "class" } } },
     { i.Type, " * @typedef %s", { type = { "type" } } },
     { nil, " * @brief $1", { type = { "func", "class", "type" } } },
@@ -14,5 +14,5 @@ return {
     { i.Tparam, " * @tparam %s $1" },
     { i.Parameter, " * @param %s $1" },
     { i.Return, " * @return $1" },
-    { nil, " */", { type = { "func", "class", "type" } } },
+    { nil, " ***************************************************************/", { type = { "func", "class", "type" } } },
 }
